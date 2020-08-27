331f8363858696d064c50f678dab26d19413c259
Dayton Gatewood <dayton.gatewood@my.simpson.edu>
2020-08-27 15:37:11 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,2 +1,4 @@
-hi
-test
+print("Hello, my name is Dayton")
+print("I go to Simpson College in Iowa")
+print("I live in a town called \"Palmer\" Alaska.")
+print("My dog's name is Maci")
---
+++ Scratch Work/Printstatements.py/main.py
@@ -0,0 +1,3 @@
+print("hello World!")
+print( "Hello, I go to Simpson College")
+print("My name is Dayton")
