9c60fdbd92a59e88ef00f54bd0150f02c039db97
Dayton Gatewood <dayton.gatewood@my.simpson.edu>
2020-10-14 14:46:18 -0500

    lab.08

---
+++ Lab 08 - Sprites/lab_08.py
@@ -0,0 +1,170 @@
+""" Sprite Sample Program """
+
+import random
+import arcade
+
+# --- Constants ---
+SPRITE_SCALING_PLAYER = 0.7
+SPRITE_SCALING_BANANA = 0.2
+BANANA_COUNT = 80
+SPRITE_SCALING_SNAKE = 0.4
+SNAKE_COUNT = 30
+
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 800
+
+
+class Banana(arcade.Sprite):
+    def __init__(self, filename, sprite_scaling):
+
+        super().__init__(filename, sprite_scaling)
+
+        self.magical_sound = arcade.load_sound("magical_1.ogg")
+
+        self.change_y = 0
+        self.change_x = 0
+
+    def update(self):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+
+        if self.left < 0:
+            self.change_x *= -1
+
+        if self.right > SCREEN_WIDTH:
+            self.change_x *= -1
+
+        if self.bottom < 0:
+            self.change_y *= -1
+
+        if self.top > SCREEN_HEIGHT:
+            self.change_y *= -1
+
+
+class Snake(arcade.Sprite):
+    def __init__(self, filename, sprite_scaling):
+        super().__init__(filename, sprite_scaling)
+
+        def update(self):
+            self.center_y -= 1
+
+            if self.top < 0:
+                self.center_y = random.randrange(SCREEN_HEIGHT + 20, SCREEN_HEIGHT + 40)
+                self.center_x = random.randrange(SCREEN_WIDTH)
+
+
+class MyGame(arcade.Window):
+    """ Our custom Window Class"""
+
+    def __init__(self):
+        """ Initializer """
+        # Call the parent class initializer
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+
+        # Variables that will hold sprite lists
+        self.player_list = None
+        self.banana_list = None
+        self.snake_list = None
+
+        # Set up the player info
+        self.player_sprite = None
+        self.score = 0
+
+        # Don't show the mouse cursor
+        self.set_mouse_visible(False)
+
+        arcade.set_background_color(arcade.color.GO_GREEN)
+
+        # Sounds from OpenGameArt.org
+        self.good_sound = arcade.load_sound("sd_0.wav")
+        self.bad_sound = arcade.load_sound("MS_Realization.wav")
+
+    def setup(self):
+        """ Set up the game and initialize the variables. """
+
+        # Sprite lists
+        self.player_list = arcade.SpriteList()
+        self.banana_list = arcade.SpriteList()
+        self.snake_list = arcade.SpriteList()
+        # Score
+
+        self.score = 0
+
+        # Elephant image from kenney.nl
+        self.player_sprite = arcade.Sprite("elephant.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 60
+        self.player_sprite.center_y = 60
+        self.player_list.append(self.player_sprite)
+
+        for i in range(BANANA_COUNT):
+            # banana image from icon-library.com
+            banana = Banana("banana.png", SPRITE_SCALING_BANANA)
+
+            banana.center_x = random.randrange(SCREEN_WIDTH)
+            banana.center_y = random.randrange(SCREEN_HEIGHT)
+            banana.change_x = random.randrange(-3, 4)
+            banana.change_y = random.randrange(-3, 4)
+
+            self.banana_list.append(banana)
+
+        for i in range(SNAKE_COUNT):
+            # banana image from kenny.nl
+            snake = Snake("snake.png", SPRITE_SCALING_SNAKE)
+
+            snake.center_x = random.randrange(SCREEN_WIDTH)
+            snake.center_y = random.randrange(SCREEN_HEIGHT)
+
+
+            self.snake_list.append(snake)
+
+    def on_draw(self):
+        arcade.start_render()
+
+        self.banana_list.draw()
+        self.player_list.draw()
+        self.snake_list.draw()
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 36)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """"Handle Mouse Motion"""
+
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+        """Movement and game logic"""
+        self.banana_list.update()
+
+        bananas_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.banana_list)
+
+        for banana in bananas_hit_list:
+            banana.remove_from_sprite_lists()
+            self.score += 1
+            arcade.play_sound(self.good_sound)
+
+        self.snake_list.update()
+
+        snake_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.snake_list)
+
+        for snake in snake_hit_list:
+            snake.remove_from_sprite_lists()
+            self.score -= 1
+            arcade.play_sound(self.bad_sound)
+
+        if len(self.banana_list) == 0:
+            output = f"Game over."
+            arcade.draw_text(output, 500, 500, arcade.color.WHITE, 40)
+
+def main():
+    """ Main method """
+    window = MyGame()
+
+    window.setup()
+
+    arcade.run()
+
+
+if __name__ == "__main__":
+    main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,135 +1,114 @@
 """ Sprite Sample Program """
 
-import random
 import arcade
 
 # --- Constants ---
+SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.5
-SPRITE_SCALING_COIN = 0.2
-COIN_COUNT = 50
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-
-class Coin(arcade.Sprite):
-
-    def reset(self):
-        self.bottom = random.randrange (SCREEN_HEIGHT, SCREEN_HEIGHT + 200)
-        self.center_x = random.randrange(SCREEN_WIDTH)
-
-    def update(self):
-        self.center_y -= 1
-        if self.top < 0:
-            self.reset()
-
+MOVEMENT_SPEED = 5
+VIEWPORT_MARGIN = 200
 
 class MyGame(arcade.Window):
-    """ Our custom Window Class"""
+    """ This class represents the main window of the game. """
 
     def __init__(self):
         """ Initializer """
         # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprite Example")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
 
-        # Variables that will hold sprite lists
+        # Sprite List
         self.player_list = None
-        self.coin_list = None
+        self.wall_list = None
 
-        # Set up the player info
+        # Players
         self.player_sprite = None
-        self.score = 0
 
-        # Don't show the mouse cursor
-        self.set_mouse_visible(False)
+        # Physics engine
+        self.physics_engine = None
 
-        arcade.set_background_color(arcade.color.AMAZON)
+
+        self.view_left = 0
+        self.view_bottom = 0
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
+        # Set the background color
+        arcade.set_background_color(arcade.color.AMAZON)
 
-        # Sprite lists
         self.player_list = arcade.SpriteList()
-        self.coin_list = arcade.SpriteList()
+        self.wall_list = arcade.SpriteList()
 
-        # Score
-        self.score = 0
+        self.view_left = 0
+        self.view_bottom = 0
 
-        # Set up the player
-        # Character image from kenney.nl
-        self.player_sprite = arcade.Sprite("character.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 50
-        self.player_sprite.center_y = 50
+        self.player_sprite = arcade.Sprite("character1.png", SPRITE_SCALING_PLAYER)
+        self.player_sprite.center_x = 64
+        self.player_sprite.center_y = 64
         self.player_list.append(self.player_sprite)
 
-        # Create the coins
-        for i in range(COIN_COUNT):
+        # Individually placing walls
+        wall = arcade.Sprite("box.png", SPRITE_SCALING_BOX)
+        wall.center_x = 300
+        wall.center_y = 200
+        self.wall_list.append(wall)
 
-            # Create the coin instance
-            # Coin image from kenney.nl
-            coin = arcade.Sprite("coin_01.png", SPRITE_SCALING_COIN)
+        wall = arcade.Sprite("box.png", SPRITE_SCALING_BOX)
+        wall.center_x = 364
+        wall.center_y = 200
+        self.wall_list.append(wall)
 
-            # Position the coin
-            coin.center_x = random.randrange(SCREEN_WIDTH)
-            coin.center_y = random.randrange(SCREEN_HEIGHT)
+        for x in range(173, 650, 64):
+            wall = arcade.Sprite("box.png", SPRITE_SCALING_BOX)
+            wall.center_x = x
+            wall.center_y = 350
+            self.wall_list.append(wall)
 
-            # Add the coin to the lists
-            self.coin_list.append(coin)
+        coordinate_list = [[400, 500], [470, 500], [400, 570], [470, 570]]
+        for coordinate in coordinate_list:
+            wall = arcade.Sprite("box.png", SPRITE_SCALING_BOX)
+            wall.center_x = coordinate[0]
+            wall.center_y = coordinate[1]
+            self.wall_list.append(wall)
 
-    def on_draw(self):
-        """ Draw everything """
-        arcade.start_render()
-        self.coin_list.draw()
-        self.player_list.draw()
-
-        # Put the text on the screen.
-        output = f"Score: {self.score}"
-        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
-
-    def on_mouse_motion(self, x, y, dx, dy):
-        """ Handle Mouse Motion """
-
-        # Move the center of the player sprite to match the mouse x, y
-        self.player_sprite.center_x = x
-        self.player_sprite.center_y = y
-
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        # to place individual walls
 
     def update(self, delta_time):
+        self.physics_engine.update()
 
-        """ Movement and game logic """
+    def on_draw(self):
+        arcade.start_render()
+        self.wall_list.draw()
+        self.player_list.draw()
 
+    def on_key_press(self, key, modifiers):
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
 
-        # Call update on all sprites (The sprites don't do much in this
-
-        # example though.)
-
-        self.coin_list.update()
-
-
-
-        # Generate a list of all sprites that collided with the player.
-
-        coins_hit_list = arcade.check_for_collision_with_list(self.player_sprite,
-
-                                                              self.coin_list)
-
-
-
-        # Loop through each colliding sprite, remove it, and add to the score.
-
-        for coin in coins_hit_list:
-
-            coin.reset()
-
-            self.score += 1
-
-
-
+    def on_key_release(self, key, modifier):
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 def main():
-    """ Main method """
     window = MyGame()
     window.setup()
     arcade.run()
+
+    # Can go to arcade.academy for more info, boundary check for wall check
 
 
 if __name__ == "__main__":
