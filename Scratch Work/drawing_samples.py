7ca0855822ff0f50fd2344ae506fd7f3af41f18d
Dayton Gatewood <dayton.gatewood@my.simpson.edu>
2020-10-29 16:16:45 -0500

    lab.11

--- Scratch Work/drawing_samples.py
+++ Scratch Work/drawing_samples.py
@@ -1,57 +1,96 @@
-""" Sprite Sample Program """
-
 import arcade
 
-# --- Constants ---
-SPRITE_SCALING_BOX = 0.5
-SPRITE_SCALING_PLAYER = 0.5
+# Set how many rows and columns we will have
+ROW_COUNT = 10
+COLUMN_COUNT = 10
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+# This sets the WIDTH and HEIGHT of each grid location
+WIDTH = 20
+HEIGHT = 20
 
-MOVEMENT_SPEED = 5
+# This sets the margin between each cell
+# and on the edges of the screen.
+MARGIN = 5
+
+# Do the math to figure out our screen dimensions
+SCREEN_WIDTH = HEIGHT * ROW_COUNT + MARGIN * (ROW_COUNT + 1)
+SCREEN_HEIGHT = WIDTH * COLUMN_COUNT + MARGIN * (COLUMN_COUNT + 1)
 
 
 class MyGame(arcade.Window):
-    """ This class represents the main window of the game. """
+    """
+    Main application class.
+    """
 
-    def __init__(self):
-        """ Initializer """
-        # Call the parent class initializer
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
+    def __init__(self, width, height):
+        """
+        Set up the application.
+        """
+        super().__init__(width, height)
+        # Create a 2 dimensional array. A two dimensional
+        # array is simply a list of lists.
+        self.grid = []
+        for row in range(ROW_COUNT):
+            self.grid.append([])
+            for column in range(COLUMN_COUNT):
+                self.grid[row].append(0)
+        self.grid[2][5] = 1
+        self.grid[row][column]
 
-        # Sprite List
-        self.player_list = None
-        self.wall_list = None
+        for row in self.grid:
+            print(row)
 
-        # Players
-        self.player_sprite = None
-
-        # Physics engine
-        self.physics_engine = None
-
-    def setup(self):
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
-
-        self.player_list = arcade.SpriteList()
-        self.wall_list = arcade.SpriteList()
-
-        self.score = 0
-
-        self.player_sprite = arcade.Sprite("elephant.png", SPRITE_SCALING_PLAYER)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 64
-        self.player_list.append(self.player_sprite)
+        arcade.set_background_color(arcade.color.BLACK)
 
     def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command has to happen before we start drawing
         arcade.start_render()
-        self.wall_lsit.draw()
-        slef.player_list.draw()
+
+        # Draw the grid
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                # Figure out what color to draw the box
+                if self.grid[row][column] == 0:
+                    color = arcade.color.ICTERINE
+                else:
+                    color = arcade.color.WHITE
+
+                # Do the math to figure out where the box is
+                x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
+                y = (MARGIN + HEIGHT) * row + MARGIN + HEIGHT // 2
+
+                # Draw the box
+                arcade.draw_rectangle_filled(x, y, WIDTH, HEIGHT, color)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+
+        # Change the x/y screen coordinates to grid coordinates
+        grid_x = x // (WIDTH + MARGIN)
+        grid_y = y // (HEIGHT + MARGIN)
+        self.grid[grid_y][grid_x] = 1
+        print(grid_x, grid_y)
+
+        # Make sure we are on-grid. It is possible to click in the upper right
+        # corner in the margin and go to a grid location that doesn't exist
+        if row < ROW_COUNT and column < COLUMN_COUNT:
+
+            # Flip the location between 1 and 0.
+            if self.grid[grid_y][grid_x] == 0:
+                self.grid[grid_y][grid_x] = 1
+            else:
+                self.grid[grid_y][grid_x] = 0
+
 
 def main():
-    window = MyGame()
-    window.setup()
+
+    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT)
     arcade.run()
 
 
