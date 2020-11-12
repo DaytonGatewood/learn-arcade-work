d78ee5f0991b3862d16a9339985a0e5ac0d2f44a
Dayton Gatewood <dayton.gatewood@my.simpson.edu>
2020-11-12 17:22:51 -0600

    lab.12

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -2,7 +2,7 @@ class Room:
     """
     This is a class to describe a room.
     """
-    def __init__(self, description, north, east, south, west, up, down):
+    def __init__(self, description, north, east, south, west, up, down, northeast, northwest, southeast, southwest):
         self.description = description
         self.north = north
         self.east = east
@@ -10,16 +10,14 @@ class Room:
         self.west = west
         self.up = up
         self.down = down
-        self.northeast = north_east
-        self.northwest = north_west
-        self.southwest = south_west
-        self.southwest = south_west
-
-
+        self.northeast = northeast
+        self.northwest = northwest
+        self.southeast = southeast
+        self.southwest = southwest
 
 
 class Item:
-    "This is a class to describe an item"
+    #This is a class to describe an item.
     def __init__(self, room_number, long_description, short_name):
         self.room_number = room_number
         self.long_description = long_description
@@ -29,46 +27,82 @@ class Item:
 def main():
     # This creates the rooms
     room_list = []
-    room = Room("You are in the living room. \nThere is a door to the east.", None, 1, None, None, None, None, None, 9, None, None)
+
+    room = Room("You are in the living room.\nThere are doors to the east and northwest.", None, 1, None, None, None,
+                None, None, 9, None, None)
     room_list.append(room)
-    room = Room("You are in the hallway.\nThere are doors to the north, east, and south.", 2, 4, 3, 0, None, None, None, None, None, None)
+
+    room = Room("You are in the hallway.\nThere are doors to the north, east, south, and west.", 2, 4, 3, 0, None, None,
+                None, None, None, None)
     room_list.append(room)
-    room = Room("You are in the library.\nThere is a scary book and a chair.\nThere are doors to the east and south.", None, 5, 1, None, None, 6, None, None, None, None)
+
+    room = Room("You are in the library.\nThere are doors to the east and south.", None, 5, 1, None, 6, None, None,
+                None, None, None)
     room_list.append(room)
-    room = Room("You are in the bedroom.\nThere is a closet full of skeletons and a coffin.\nThere is a door to the north.", 1, None, None, None, 7, None, None, None, None, None)
+
+    room = Room("You are in the bedroom.\nThere is a door to the north.\nThere's a ladder to go up.", 1, None, None,
+                None, 7, None, None, None, None, None)
     room_list.append(room)
-    room = Room("You are in the kitchen.\nThere is a fridge and butcher knives.\nThere are doors to the south and west.", None, None, 4, 2, None, None, None, None, None, None)
+
+    room = Room("You are in the kitchen.\nThere are doors to the north and west.", 5, None, None, 1, None, None, None,
+                None, None, None)
     room_list.append(room)
-    room = Room("You are in the dining room.\nThere is a banana and spiderwebs.\nThere is a door to the west.", None, None, None, 1, None, 6, None, None, None, None)
+
+    room = Room("You are in the dining room.\nThere are doors to the west, south, and northeast.",
+                None, None, 4, 2, None, None, 8, None, None, None)
     room_list.append(room)
-    room_list = Room("You are in the basement.\nThere is a staircase to go up.", None, None, None, None, 5, None, None, None, None, None)
+
+    room = Room("You are in the basement.\nThere is a scary beast in here.\nThere is a staircase to go up.", None, None,
+                None, None, 5, None, None, None, None, None)
     room_list.append(room)
-    room_list = Room("You are in the attic. There is a ladder under you.", None, None, None, None, None, 3, None, None, None, None)
+
+    room = Room("You are in the attic.\nThere is a ladder under you.", None, None, None, None, None, 3, None, None, None,
+                None)
     room_list.append(room)
-    room = Room("You are in the 8th room\nThere is a door southwest of you.", None, None, None, None, None, None, None, None, None, 5)
+
+    room = Room("You are in the party room.\nThere is a door southwest.", None, None, None, None, None, None, None,
+                None, None, 5)
     room_list.append(room)
-    room = Room("You are in the 9th room.\nThere is a door to the southeast.", None, None, None, None, None, None, None, 0, None, None)
+
+    room = Room("You are in the exit room.\nThere is a door to the southeast.", None, None, None, None, None, None,
+                None, 0, None, None)
     room_list.append(room)
 
     # This creates the items
     item_list = []
-    tv = Item(0, "The tv is off but you can click the button to turn it on", "button")
+
+    tv = Item(0, "The tv is off but you can click the button to turn it on.", "button")
     item_list.append(tv)
+
+    lock = Item(9, "There's the door to escape. It can be unlocked with a key.", "lock")
+    item_list.append(lock)
+
     lamp = Item(1, "There is a lamp with a chain to turn it on.", "chain")
     item_list.append(lamp)
-    key = Item(1, "There is also a key on the ground", "key")
-    item_list.append(key)
-    lever = Item(2, "There is a secret lever on the wall", "lever")
+
+    lever = Item(2, "There is a secret lever on the wall.", "lever")
     item_list.append(lever)
+
     door = Item(3, "There is a closet with the door ajar.", "door")
     item_list.append(door)
+
     cheese = Item(4, "There is a fridge.", "fridge")
     item_list.append(cheese)
-    switch = Item(5, "There is a switch on the floor", "switch")
+
+    switch = Item(3, "There is a switch on the floor.", "switch")
     item_list.append(switch)
-    sword = Item(6, "There is a sword laying on the ground.", "sword")
+
+    sword = Item(5, "There is a sword laying on the ground.", "sword")
     item_list.append(sword)
 
+    bone = Item(7, "There is a bone on the ground.", "bone")
+    item_list.append(bone)
+
+    code = Item(6, "There is a key pad on the wall behind the beast.\nInsert the code to unlock a door.", "code")
+    item_list.append(code)
+
+    cake = Item(8, "There is a cake on the table.", "cake")
+    item_list.append(cake)
 
 
     current_room = 0
@@ -127,21 +161,28 @@ def main():
                 current_room = next_room
 
         elif user_choice.lower() == "ne" or user_choice.lower() == "northeast":
-            next_room = room_list[current_room].north_east
+            next_room = room_list[current_room].northeast
             if next_room == None:
                 print("You can't go that way.")
             else:
                 current_room = next_room
 
         elif user_choice.lower() == "nw" or user_choice.lower() == "northwest":
-            next_room = room_list[current_room].north_west
+            next_room = room_list[current_room].northwest
             if next_room == None:
                 print("You can't go that way.")
             else:
                 current_room = next_room
 
         elif user_choice.lower() == "se" or user_choice.lower() == "southeast":
-            next_room = room_list[current_room].south_east
+            next_room = room_list[current_room].southeast
+            if next_room == None:
+                print("You can't go that way.")
+            else:
+                current_room = next_room
+
+        elif user_choice.lower() == "sw" or user_choice.lower() == "southwest":
+            next_room = room_list[current_room].southwest
             if next_room == None:
                 print("You can't go that way.")
             else:
@@ -151,7 +192,6 @@ def main():
             done = True
         else:
             print("I don't understand that command.")
-
 
 
 main()
